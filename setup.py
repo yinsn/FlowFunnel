d0442c4c00fb710c5d6c4ead88dd56be5a694470
chengyin <ycheng_sjtu@foxmail.com>
2023-10-11 15:30:38 +0800

    setup

---
+++ setup.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+SETUP_DIRECTORY = Path(__file__).resolve().parent
+
+with (SETUP_DIRECTORY / "README.md").open() as ifs:
+    LONG_DESCRIPTION = ifs.read()
+
+install_requires = (
+    [
+        "pymc>=5.9.0",
+    ],
+)
+
+__version__ = "0.0.1"
+
+setup(
+    name="flowfunnel",
+    version=__version__,
+    author="Yin Cheng",
+    author_email="yin.sjtu@gmail.com",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yinsn/FlowFunnel",
+    python_requires=">=3.6",
+    description="Leveraging Bayesian hierarchical models to diagnose issues and proactively predict user flow stages in conversion funnels.",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    install_requires=install_requires,
+    include_package_data=True,
+)
