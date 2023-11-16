a8359b302ec4574b81cfc31eed9968d78ed943d6
chengyin <ycheng_sjtu@foxmail.com>
2023-11-17 01:00:04 +0800

    adapt_diag init

--- src/flowfunnel/funnels/base.py
+++ src/flowfunnel/funnels/base.py
@@ -98,8 +98,8 @@ class BaseFunnel(ABC):
     def update_data_block(
         self,
         data_block: List[np.ndarray],
-        samples: int = 1000,
-        tune: int = 1000,
+        samples: int = 500,
+        tune: int = 100,
         cores: Optional[int] = None,
         chains: Optional[int] = None,
     ) -> None:
@@ -112,8 +112,8 @@ class BaseFunnel(ABC):
 
         Args:
             data_block (List[np.ndarray]): A list of numpy arrays representing the data block.
-            samples (int, optional): Number of samples to draw. Defaults to 1000.
-            tune (int, optional): Number of iterations to tune. Defaults to 1000.
+            samples (int, optional): Number of samples to draw. Defaults to 500.
+            tune (int, optional): Number of iterations to tune. Defaults to 100.
             cores (Optional[int], optional): Number of cores to run the sampling on. Defaults to None.
             chains (Optional[int], optional): Number of chains to run. Defaults to None.
         """
@@ -122,4 +122,7 @@ class BaseFunnel(ABC):
             pm.set_data(
                 new_data=constant_data_dict,
             )
-            self.new_trace = self.generate_trace(samples, tune, cores, chains)
+            self.new_trace = pm.sample(
+                samples, tune=tune, cores=cores, chains=chains, init="adapt_diag"
+            )
+            self.new_summary = pm.summary(self.new_trace).round(3)
