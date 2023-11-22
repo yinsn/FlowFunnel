d2073c8bf80be52fdadfda39c36791a68cf61d9c
chengyin <ycheng_sjtu@foxmail.com>
2023-11-22 15:48:25 +0800

    simply funnels

--- src/flowfunnel/funnels/__init__.py
+++ src/flowfunnel/funnels/__init__.py
@@ -1,11 +1,12 @@
-from .base import BaseFunnel
-from .graph_funnel import GraphFunnel
+# from .base import BaseFunnel
+# from .graph_funnel import GraphFunnel
 from .pyro_funnel import PyroFunnel
-from .sequential_funnel import SequentialFunnel
+
+# from .sequential_funnel import SequentialFunnel
 
 __all__ = [
-    "BaseFunnel",
-    "GraphFunnel",
+    # "BaseFunnel",
+    # "GraphFunnel",
     "PyroFunnel",
-    "SequentialFunnel",
+    # "SequentialFunnel",
 ]
