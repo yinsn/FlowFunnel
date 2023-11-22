48b5b351e29b4cbaade0d051bc7251beeb5ff077
chengyin <ycheng_sjtu@foxmail.com>
2023-11-22 16:00:24 +0800

    simplify layers

--- src/flowfunnel/layers/__init__.py
+++ src/flowfunnel/layers/__init__.py
@@ -1,25 +1,27 @@
-from .AR1_binary import AR1BinaryLayer
-from .AR1_int import AR1IntLayer
-from .ARn_binary import ARnBinaryLayer
-from .ARn_float import ARnFloatLayer
-from .ARn_int import ARnIntLayer
+# from .AR1_binary import AR1BinaryLayer
+# from .AR1_int import AR1IntLayer
+# from .ARn_binary import ARnBinaryLayer
+# from .ARn_float import ARnFloatLayer
+# from .ARn_int import ARnIntLayer
 from .ARn_pyro import ARnPyroLayer
-from .base import BaseLayer
+
+# from .base import BaseLayer
 from .base_pyro import BasePyroLayer
-from .decision import DecisionLayer
-from .GRW_to_bernoulli import GRWToBernoulliLayer
-from .GRW_to_poisson import GRWToPoissonLayer
+
+# from .decision import DecisionLayer
+# from .GRW_to_bernoulli import GRWToBernoulliLayer
+# from .GRW_to_poisson import GRWToPoissonLayer
 
 __all__ = [
-    "AR1BinaryLayer",
-    "AR1IntLayer",
-    "ARnBinaryLayer",
-    "ARnFloatLayer",
-    "ARnIntLayer",
+    # "AR1BinaryLayer",
+    # "AR1IntLayer",
+    # "ARnBinaryLayer",
+    # "ARnFloatLayer",
+    # "ARnIntLayer",
     "ARnPyroLayer",
-    "BaseLayer",
+    # "BaseLayer",
     "BasePyroLayer",
-    "DecisionLayer",
-    "GRWToBernoulliLayer",
-    "GRWToPoissonLayer",
+    # "DecisionLayer",
+    # "GRWToBernoulliLayer",
+    # "GRWToPoissonLayer",
 ]
