39c8b5e9aa49ff9e48427a1e8d611c20732de2c1
chengyin <ycheng_sjtu@foxmail.com>
2023-12-19 16:31:06 +0800

    Removed  abstract method.

--- src/flowfunnel/layers/base_pyro.py
+++ src/flowfunnel/layers/base_pyro.py
@@ -22,15 +22,3 @@ class BasePyroLayer(ABC):
         self.name: str = name
         self.raw_data: List[float] = data
         self.state: float = sum(data) / len(data) if data else 0
-
-    @abstractmethod
-    def update_state(self, t: int) -> None:
-        """Abstract method to update the state of the layer.
-
-        This method should be implemented in subclasses to define the logic for
-        updating the layer's state based on new data or other factors.
-
-        Args:
-            t (int): The time step or other relevant index for updating the state.
-        """
-        pass
